1e7300b50c7bed2311ef2664888c60855950e9cd
yngvemag <yngvem@gmail.com>
2025-11-13 13:32:27 +0100

    updated week46

---
+++ week46/BlazorPlayGround/Components/Pages/07_CascadingParameters/CascadingParametersDemo.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+namespace BlazorPlayGround.Components.Pages._07_CascadingParameters;
+
+public partial class CascadingParametersDemo : ComponentBase
+{
+    private CascadingState CascadingStateValue { get; set; } = new CascadingState();
+
+    private void OnThemeChanged(ChangeEventArgs e)
+    {
+        CascadingStateValue.Theme = e.Value?.ToString() ?? "light";
+    }
+
+    private void OnTextSizeChanged(ChangeEventArgs e)
+    {
+
+    }
+
+    public class CascadingState
+    {
+        public string Theme { get; set; } = "light";
+        public string TextSize { get; set; } = "medium";
+    }
+}
